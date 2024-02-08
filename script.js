b85aa23dfa9c0e103cce4704ced053f4d03347ad
Ruthishkumarr <ruthishkumarrajendran@gmail.com>
2024-02-08 18:34:40 +0530

    Call Back Updated

---
+++ script.js
@@ -0,0 +1,34 @@
+const head = document.getElementById("head");
+setTimeout(function () {
+  head.innerText=("10");
+  setTimeout(function () {
+    head.innerText=("9");
+    setTimeout(function () {
+      head.innerText=("8");
+      setTimeout(function () {
+        head.innerText=("7");
+        setTimeout(function () {
+          head.innerText=("6");
+          setTimeout(function () {
+            head.innerText=("5");
+            setTimeout(function () {
+              head.innerText=("4");
+              setTimeout(function () {
+                head.innerText=("3");
+                setTimeout(function () {
+                  head.innerText=("2");
+                  setTimeout(function () {
+                    head.innerText=("1");
+                    setTimeout(function () {
+                      head.innerText=("HAPPY INDEPENDENCE DAY");
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
